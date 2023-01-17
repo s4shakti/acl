649dc1a40ea7806f221381727ea0a73057c83258
Shaktidhar Goswami <Shaktidhar.goswami@velocis.in>
2023-01-17 15:50:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./frontend/shared/Header";
+import Footer from "./frontend/shared/Footer";
+import AppRouting from "./frontend/AppRouting";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AppRouting />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
